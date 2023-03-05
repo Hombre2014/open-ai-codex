b6d8ef2dea855b80c628eb9ee96b9994fbd29ff4
Yuriy Chamkoriyski <chamkoriyski@gmail.com>
2023-03-05 17:45:11 +0100

    Change the server URL to render live URL

--- client/script.js
+++ client/script.js
@@ -84,7 +84,7 @@ const handleSubmit = async (e) => {
   // messageDiv.innerHTML = "..."
   loader(messageDiv);
 
-  const response = await fetch('http://localhost:5000', {
+  const response = await fetch('https://codex-u9ty.onrender.com/', {
     method: 'POST',
     headers: {
       'Content-Type': 'application/json',
